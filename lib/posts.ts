20ac9a378a0f09a46cd23d3a08d3f9729510dcee
comtlee <comt.mix@gmail.com>
2026-02-23 15:57:44 +0900

    fix: 블로그 등록하는 폴더명 수정

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), 'content/posts');
+const postsDirectory = path.join(process.cwd(), 'content/blogs');
 
 export interface PostMeta {
   slug: string;
